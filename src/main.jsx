6457b10a20daa8ad0f1688f345729b69f81fcaeb
desarrollo <desarrollo@academicos.cesde.edu.co>
2025-02-04 07:27:30 -0500

    se crean los componentes

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
 
-
+import { Menu } from './components/common/Menu/Menu'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-
+    <Menu></Menu>
   </StrictMode>,
 )
